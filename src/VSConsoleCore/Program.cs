40982c5988d6dcc6b1809ca1dbaed55431a432f5
Grace Dong (CSI Interfusion Inc) <v-grdong@microsoft.com>
2020-05-06 09:37:12 +0000

    add some code

--- src/VSConsoleCore/Program.cs
+++ src/VSConsoleCore/Program.cs
@@ -6,7 +6,7 @@ namespace VSOConsoleCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Nexus!");
         }
     }
 }
